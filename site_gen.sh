4e756d824285bc1c8fbd37a12302ba881307bfd3
melserngawy <melserng@redhat.com>
2021-02-03 13:05:05 -0500

    amend
    Signed-off-by: melserngawy <melserng@redhat.com>

--- site_gen.sh
+++ site_gen.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
 CLUSTER_NAME=$1
-profile=$2
+PROFILE=$2
+RAN_MANIFEST_REPO=$3
+ORG_RAN_MANIFEST_REPO=https://github.com/redhat-ztp/ztp-ran-manifests
 
-if [ $profile == "cu" ]; then
+if [ $PROFILE == "cu" ]; then
   cp -rf subscriptions/sites/sample_site_cu_subscription subscriptions/sites/${CLUSTER_NAME}_subscription
   sed -i "s/sample-site-cu/$CLUSTER_NAME/g" subscriptions/sites/${CLUSTER_NAME}_subscription/*
-elif [ $profile == "du" ]; then
+elif [ $PROFILE == "du" ]; then
   cp -rf subscriptions/sites/sample_site_du_subscription subscriptions/sites/${CLUSTER_NAME}_subscription
   sed -i "s/sample-site-du/$CLUSTER_NAME/g" subscriptions/sites/${CLUSTER_NAME}_subscription/*
 else
-  echo "Profile should be either cu or du. Profile=" $profile
+  echo "Profile should be either cu or du. Profile=" $PROFILE
+fi
+
+if [ ! -z $RAN_MANIFEST_REPO ]; then
+  for f in $(find subscriptions/ -name *.yaml); do
+    #sed --debug -i "s,$ORG_RAN_MANIFEST_REPO,$RAN_MANIFEST_REPO,g" $f
+    sed -i "s,$ORG_RAN_MANIFEST_REPO,$RAN_MANIFEST_REPO,g" $f
+  done
 fi
